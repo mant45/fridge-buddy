17283edcf5d4051c39c6e9bf6a2ed13c2b4abb7f
kennethgargan <kennethgargan@gmail.com>
2022-06-13 15:50:10 +0100

    Updated front-end

---
+++ docs/main.js
@@ -0,0 +1,17 @@
+//# Form
+function checkForm(){
+    let term = document.getElementById("ingredient_entry").value;
+    
+    if(term===""){
+        alert("Please Enter in term to search for.");
+    }
+    else{
+        document.getElementById("results_heading").innerHTML = ("Results for:" + term);
+        document.getElementById("results").classList.add("fadeIn");
+    }
+}
+
+//When our submit button is clicked
+document.getElementById('submit').addEventListener("click", function(){
+    checkForm();
+});
