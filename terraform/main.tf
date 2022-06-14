c53fdbc199751aca84a10459b2c3ebc7043672aa
mant45 <mantaspostbox@gmail.com>
2022-06-14 17:09:32 +0100

    remove not needed permissions for lambda

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,61 +37,21 @@ resource "aws_iam_role_policy" "read_dynamo_db" {
     Statement = [
       {
         Action = [
-          "application-autoscaling:DescribeScalableTargets",
-          "application-autoscaling:DescribeScalingActivities",
-          "application-autoscaling:DescribeScalingPolicies",
-          "cloudwatch:DescribeAlarmHistory",
-          "cloudwatch:DescribeAlarms",
-          "cloudwatch:DescribeAlarmsForMetric",
-          "cloudwatch:GetMetricStatistics",
-          "cloudwatch:ListMetrics",
-          "cloudwatch:GetMetricData",
-          "datapipeline:DescribeObjects",
-          "datapipeline:DescribePipelines",
-          "datapipeline:GetPipelineDefinition",
-          "datapipeline:ListPipelines",
-          "datapipeline:QueryObjects",
-          "dynamodb:BatchGetItem",
-          "dynamodb:Describe*",
-          "dynamodb:List*",
           "dynamodb:GetItem",
           "dynamodb:Query",
-          "dynamodb:Scan",
-          "dynamodb:PartiQLSelect",
-          "dax:Describe*",
-          "dax:List*",
-          "dax:GetItem",
-          "dax:BatchGetItem",
-          "dax:Query",
-          "dax:Scan",
-          "ec2:DescribeVpcs",
-          "ec2:DescribeSubnets",
-          "ec2:DescribeSecurityGroups",
-          "iam:GetRole",
-          "iam:ListRoles",
-          "kms:DescribeKey",
-          "kms:ListAliases",
-          "sns:ListSubscriptionsByTopic",
-          "sns:ListTopics",
-          "lambda:ListFunctions",
-          "lambda:ListEventSourceMappings",
-          "lambda:GetFunctionConfiguration",
-          "resource-groups:ListGroups",
-          "resource-groups:ListGroupResources",
-          "resource-groups:GetGroup",
-          "resource-groups:GetGroupQuery",
-          "tag:GetResources",
-          "kinesis:ListStreams",
-          "kinesis:DescribeStream",
-          "kinesis:DescribeStreamSummary"
+          "dynamodb:Scan"
         ]
         Effect   = "Allow"
-        Resource = "*"
+        Resource = "${aws_dynamodb_table.food_table.arn}"
       },
       {
-        Action   = "cloudwatch:GetInsightRuleReport",
-        Effect   = "Allow",
-        Resource = "arn:aws:cloudwatch:*:*:insight-rule/DynamoDBContributorInsights*"
+        Action: [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+        ]
+        Effect: "Allow"
+        Resource: "*"
       }
     ]
   })
