1cdad9f98964732999fc3d53325a9802d49f4f57
kennethgargan <kennethgargan@gmail.com>
2022-06-15 12:16:57 +0100

    Updated Lambda code

--- terraform/python/lambda_code.py
+++ terraform/python/lambda_code.py
@@ -1,7 +1,17 @@
 import json
+import json
+import boto3
+
 
 def lambda_handler(event, context):
-    return {
-        'statusCode': 200,
-        'body': json.dumps(event['headers']['X-Forwarded-For'])
-    }
+
+    if event['rawPath'] == "/request":
+        item = event['queryStringParameters']["ingredient"]
+        
+        #Create our Boto3 client & Set our Table name
+        client = boto3.resource("dynamodb")
+        table = client.Table("recipe")
+
+        #Get 1 item of the database
+        item = table.get_item(Key={"name":"Kenneth"})
+        return item
