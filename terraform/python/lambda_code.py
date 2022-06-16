20f11c77c20617244565d66cbf2c81b4bbbe6290
kennethgargan <kennethgargan@gmail.com>
2022-06-16 12:37:38 +0100

    Updated DynamoDB Search

--- terraform/python/lambda_code.py
+++ terraform/python/lambda_code.py
@@ -1,15 +1,14 @@
 import json
 import boto3
+from boto3.dynamodb.conditions import Key, Attr
 
 
 def lambda_handler(event, context):
-
-    
     item = event['queryStringParameters']["ingredient"]
         
     #Create our Boto3 client & Set our Table name
     client = boto3.resource("dynamodb")
     table = client.Table("food-table")
 
-    #Get 1 item of the database
-    return table.get_item(Key={"ingredient_1":item})
+    #Get all items of the database
+    return json.dumps(table.scan(FilterExpression=Attr('ingredient_1').contains('item')))
