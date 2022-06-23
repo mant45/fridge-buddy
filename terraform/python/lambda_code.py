5fefd4ca044c86413f1773d7f7ba29705a096d4d
kennethgargan <kennethgargan@gmail.com>
2022-06-23 10:02:15 +0100

    Updated code to handle certain types

--- terraform/python/lambda_code.py
+++ terraform/python/lambda_code.py
@@ -1,7 +1,7 @@
 import json
 import boto3
 from boto3.dynamodb.conditions import Key, Attr
-
+from decimal import *
 
 def lambda_handler(event, context):
     item = event['queryStringParameters']["ingredient"]
@@ -10,5 +10,14 @@ def lambda_handler(event, context):
     client = boto3.resource("dynamodb")
     table = client.Table("food-table")
 
-    #Get keyword filtered items of the database
-    return table.scan(FilterExpression=Attr('ingredients_main').contains(item))
+    #Get the data that matches & create a payload of what we want to send back to the client
+    data = table.scan(FilterExpression=Attr('ingredients_main').contains(item))
+    payload = json.dumps(data["Items"], cls=DecimalEncoder)
+    return payload
+
+#Since JSON doesn't handle decimals, we need to encode it to another format.    
+class DecimalEncoder(json.JSONEncoder):
+    def default(self, obj):
+        if isinstance(obj, Decimal):
+            return str(obj)
+        return json.JSONEncoder.default(self, obj)
